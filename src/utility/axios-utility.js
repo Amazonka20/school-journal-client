0ed5857e865098065d9a84a19a4a2e2f2edbc7fb
Amazonka20 <polinavinnikova0@gmail.com>
2021-03-21 15:27:32 +0200

    Change link to service.

--- src/utility/axios-utility.js
+++ src/utility/axios-utility.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: "https://k2y9oghiva.execute-api.eu-central-1.amazonaws.com/dev",
-    baseURL: "http://192.168.0.102:8080/",
+    baseURL: "https://k2y9oghiva.execute-api.eu-central-1.amazonaws.com/dev/api",
 });
 
 export const saveToken = (token) => {
