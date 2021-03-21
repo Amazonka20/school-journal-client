39d40d386a56e4142fd6082916a1d6ca78e8b147
Amazonka20 <polinavinnikova0@gmail.com>
2021-03-21 21:24:02 +0200

    Change service url

--- src/utility/axios-utility.js
+++ src/utility/axios-utility.js
@@ -1,15 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: "https://k2y9oghiva.execute-api.eu-central-1.amazonaws.com/dev/api",
-    baseURL: "http://localhost:8080/",
+    baseURL: "https://k2y9oghiva.execute-api.eu-central-1.amazonaws.com/dev/api",
 });
-
-// export const saveToken = (token) => {
-//     axios.interceptors.request.use(req => {
-//         req.headers.authorization = token;
-//         return req;
-//     });
-// }
 
 export default instance;
